b727a803ae9c926fd1adddad3bee78608d23c80e
MarkSemaan <mark.h.semaan@gmail.com>
2025-07-03 23:25:07 +0300

    created categories table and added foreign key to articles migration

--- migrations/001_create_articles_table.php
+++ migrations/001_create_articles_table.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 require("../connection/connection.php");
 
 
@@ -6,7 +6,9 @@ $query = "CREATE TABLE articles(
           id INT(11) AUTO_INCREMENT PRIMARY KEY, 
           name VARCHAR(255) NOT NULL, 
           author VARCHAR(255) NOT NULL, 
-          description TEXT NOT NULL)";
+          description TEXT NOT NULL,
+          category_id INT(11),
+          FOREIGN KEY (category_id) REFERENCES categories(id))";
 
 $execute = $mysqli->prepare($query);
 $execute->execute();
---
+++ migrations/003_create_categories_table.php
@@ -0,0 +1,7 @@
+<?php
+
+require('./connection/connection.php');
+
+$query = "CREATE TABLE categories(
+            id INT(11)AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(255) NOT NULL)";

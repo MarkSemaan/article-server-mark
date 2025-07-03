7f1f14c81e6a2a732688a6b75c971c17f810f0b5
MarkSemaan <mark.h.semaan@gmail.com>
2025-07-03 23:28:20 +0300

    refactor Article and base Model classes; add Category model

--- models/Article.php
+++ models/Article.php
@@ -1,52 +1,73 @@
 <?php
 require_once("Model.php");
 
-class Article extends Model{
+class Article extends Model
+{
 
-    private int $id; 
-    private string $name; 
-    private string $author; 
-    private string $description; 
-    
+    private int $id;
+    private string $name;
+    private string $author;
+    private string $description;
+    private int $category_id;
+
     protected static string $table = "articles";
 
-    public function __construct(array $data){
+    public function __construct(array $data)
+    {
         $this->id = $data["id"];
         $this->name = $data["name"];
         $this->author = $data["author"];
         $this->description = $data["description"];
+        $this->category_id = $data["category_id"];
     }
 
-    public function getId(): int {
+    public function getId(): int
+    {
         return $this->id;
     }
+    public function getCategoryId(): int
+    {
+        return $this->category_id;
+    }
 
-    public function getName(): string {
+    public function getName(): string
+    {
         return $this->name;
     }
 
-    public function getAuthor(): string {
+    public function getAuthor(): string
+    {
         return $this->author;
     }
 
-    public function getDescription(): string {
+    public function getDescription(): string
+    {
         return $this->description;
     }
 
-    public function setName(string $name){
+    public function setName(string $name)
+    {
         $this->name = $name;
     }
 
-    public function setAuthor(string $author){
+    public function setAuthor(string $author)
+    {
         $this->author = $author;
     }
 
-    public function setDescription(string $description){
+    public function setDescription(string $description)
+    {
         $this->description = $description;
     }
 
-    public function toArray(){
-        return [$this->id, $this->name, $this->author, $this->description];
+    public function toArray()
+    {
+        return [
+            "id" => $this->id,
+            "name" => $this->name,
+            "author" => $this->author,
+            "description" => $this->description,
+            "category_id" => $this->category_id
+        ];
     }
-    
 }
